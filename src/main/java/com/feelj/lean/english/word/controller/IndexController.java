70024fd93e62985b399a4c5ee7e5fddb9427d3ea
feelj <feelingjos@163.com>
2019-07-10 23:23:29 +0800

    去除多余

--- src/main/java/com/feelj/lean/english/word/controller/IndexController.java
+++ src/main/java/com/feelj/lean/english/word/controller/IndexController.java
@@ -1,17 +1,11 @@
 package com.feelj.lean.english.word.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.feelj.lean.english.word.entity.Test;
-import com.feelj.lean.english.word.service.TestService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
-
 /**
  * @Auther: feelj
  * @Date: 2019/5/9 09:28
@@ -20,17 +14,9 @@ import java.util.List;
 @RestController
 public class IndexController {
 
-    @Autowired
-    TestService testService;
-
     @GetMapping("/index")
     public ModelAndView index(HttpServletRequest request,HttpServletResponse response){
 
-        List<Test> all = testService.findAll();
-
-        String s = JSON.toJSONString(all);
-
-        request.setAttribute("test",all.get(0));
         return new ModelAndView("index");
     }
 

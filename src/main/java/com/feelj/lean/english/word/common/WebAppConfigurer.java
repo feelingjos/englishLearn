c0e30b28f716f8f1fb6bfbbabdd845eb5cc191f5
feelj <feelingjos@163.com>
2019-08-05 23:35:43 +0800

    路径映射

---
+++ src/main/java/com/feelj/lean/english/word/common/WebAppConfigurer.java
@@ -0,0 +1,25 @@
+package com.feelj.lean.english.word.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
+
+/**
+ * @Auther: feelj
+ * @Date: 2019/8/5 23:24
+ * @Description:  路径映射
+ */
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String path = System.getProperty("user.dir") + File.separator;
+
+        System.out.println(path);
+
+        registry.addResourceHandler("/res/**").addResourceLocations("file:"+path);
+    }
+}
